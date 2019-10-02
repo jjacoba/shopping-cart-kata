c73fb502ed9e0daa1e1f9fd49f46f7e8e2b505b2
Jacob Adam Nevitt <jacobadamnevitt@Jacobs-MacBook-Pro.local>
2019-10-02 20:47:56 +0100

    trying to work out the logic for the last test

--- checkout.js
+++ checkout.js
@@ -10,19 +10,32 @@ class Checkout {
   }
 
   total() {
-    if (this.basket.A > 0) {
+    if (this.basket.A > 0 && this.basket.B > 0) {
+      this.currentTotal +=
+        Math.floor(this.basket.A / 3) * 140 + (this.basket.A % 3) * 50;
+        this.currentTotal +=
+          Math.floor(this.basket.B / 2) * 60 + (this.basket.B % 2) * 35
+          + this.basket.C * 25 + this.basket.D * 12;
+          console.log('A + B')
+    }
+    if (this.basket.A > 0 && this.basket.B === 0) {
       this.currentTotal +=
         Math.floor(this.basket.A / 3) * 140 + (this.basket.A % 3) * 50
         + this.basket.C * 25 + this.basket.D * 12
+          console.log("NO B");
+
     }
-    if (this.basket.B > 0) {
+    if (this.basket.B > 0 && this.basket.A === 0) {
       this.currentTotal +=
         Math.floor(this.basket.B / 2) * 60 + (this.basket.B % 2) * 35
-        + this.basket.C * 25 + this.basket.D * 12;
+          console.log("NO A");
+
     } else {
-      if (!this.basket.A && !this.basket.B) 
+      console.log(this.basket.A, this.basket.B)
+     if (this.basket.A === 0 && this.basket.B === 0) 
       this.currentTotal += this.basket.C * 25;
       this.currentTotal += this.basket.D * 12;
+          console.log("NO A OR B");
     }
     return this.currentTotal;
   }
